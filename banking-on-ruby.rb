9b3e0afc51967cda9dfad54c9359f471ec427fd1
philiptang42 <philiptang42@gmail.com>
2016-01-27 16:47:57 -0500

    solution

--- banking-on-ruby.rb
+++ banking-on-ruby.rb
@@ -6,15 +6,24 @@ class Account
   end
 
   def display_balance(pin_number)
-    if pin_number == @pin
+    if pin_number == pin
       puts "Balance: $#{@balance}"
     else
       pin_error
     end
   end
 
+  def withdraw(pin_number, amount)
+    if pin_number == pin
+      balance - amount
+      puts "Withdrew $#{amount}. New balance: $#{@balance}."
+    else
+      pin_error
+    end
+  end
+
   private
-  
+
   def pin
     @pin = 1234
   end
@@ -22,4 +31,11 @@ class Account
   def pin_error
     return "Access denied: incorrect PIN."
   end
+
 end
+
+checking_account = Account.new("Dude")
+
+puts checking_account.display_balance(0000)
+puts checking_account.display_balance(1234)
+puts checking_account.withdraw(1234, 50)
